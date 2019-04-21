99b87c3bfe4f414aed8dd1aaff9eb28c285fd5bf
Rodrigo Prado <royopa@gmail.com>
2019-04-21 07:18:49 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,3 +22,18 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+import pandas as pd
+
+# dados da petrobrás a partir de 2018-09-18
+url = 'http://www.petrobras.com.br/lumis/api/rest/pricegraphnovo/pricedata?n=4'
+df = pd.read_json(url)
+df.columns = ['data_publicacao', 'data_variacao', 'vr_diesel', 'vr_gasolina']
+print('Saving info from {}'.format(data_publicacao)
+for index, row in df.iterrows():
+  scraperwiki.sqlite.save(unique_keys=['data_publicacao'], data={
+    'data_publicacao': row['data_publicacao'],
+    'data_variacao': row['data_variacao'],
+    'vr_diesel': row['vr_diesel'],
+    'vr_gasolina': row['vr_gasolina']
+  })
