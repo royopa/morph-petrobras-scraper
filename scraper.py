c74652d0ca39e1477d57cb1065095a448426b1f8
Rodrigo Prado <royopa@gmail.com>
2019-04-21 07:32:03 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -29,11 +29,13 @@ import pandas as pd
 url = 'http://www.petrobras.com.br/lumis/api/rest/pricegraphnovo/pricedata?n=4'
 df = pd.read_json(url)
 df.columns = ['data_publicacao', 'data_variacao', 'vr_diesel', 'vr_gasolina']
-print('Saving info from {}'.format(data_publicacao)
+
+print('Saving info from {}'.format(data_publicacao))
+
 for index, row in df.iterrows():
-  scraperwiki.sqlite.save(unique_keys=['data_publicacao'], data={
-    'data_publicacao': row['data_publicacao'],
-    'data_variacao': row['data_variacao'],
-    'vr_diesel': row['vr_diesel'],
-    'vr_gasolina': row['vr_gasolina']
-  })
+    scraperwiki.sqlite.save(unique_keys=['data_publicacao'], data={
+      'data_publicacao': row['data_publicacao'],
+      'data_variacao': row['data_variacao'],
+      'vr_diesel': row['vr_diesel'],
+      'vr_gasolina': row['vr_gasolina']
+    })
